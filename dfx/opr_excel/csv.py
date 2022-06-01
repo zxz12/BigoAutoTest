64740cc50f4af3659573b8afa0e5b5db512e2e50
zhangxianzhi <zhangxianzhi@bigo.sg>
2022-06-01 14:08:03 +0800

    zxz

--- dfx/opr_excel/csv.py
+++ dfx/opr_excel/csv.py
@@ -7,5 +7,5 @@ from dfx.CONSTANT import CONSTANT
 def write_csv(header, datas):
     path = os.path.join(CONSTANT.RES_PATH, 'debt_asset_ratio.csv')
     pd_f = pd.DataFrame(datas)
-    f = pd_f.to_csv(path, header=header, index=False,encoding='utf-8')
+    f = pd_f.to_csv(path, header=header, index=False,encoding='utf_8_sig')
     return f
